236f92eeaebf87b82630532a6cfa796402f0379f
Franco Panvini <francopanvini@gmail.com>
2021-08-30 23:54:26 -0300

    Add route getRecipes error handling

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -11,13 +11,17 @@ recipes.get("/", async (req, res) => {
   const { name } = req.query;
 
   let dbRecipes = await getDBRecipes();
+  if (dbRecipes instanceof Error) res.status(400).send(dbRecipes.message);
   dbRecipes = dbRecipes.map(recipe => {
     return { ...recipe, source: "db" };
   });
+
   let spoonRecipes = await getRecipes(true, false, 300);
+  if (spoonRecipes instanceof Error) res.status(400).send(spoonRecipes.message);
   spoonRecipes = spoonRecipes.map(recipe => {
     return { ...recipe, source: "spoon" };
   });
+
   let recipes = [...dbRecipes, ...spoonRecipes];
   if (name) {
     recipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(name.toLowerCase()));
@@ -28,6 +32,7 @@ recipes.get("/", async (req, res) => {
 recipes.get("/db", async (req, res) => {
   const { name } = req.query;
   let recipes = await getDBRecipes();
+  if (recipes instanceof Error) res.status(400).send(recipes.message);
   if (name) {
     recipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(name.toLowerCase()));
   }
@@ -37,11 +42,17 @@ recipes.get("/db", async (req, res) => {
 recipes.get("/spoon", async (req, res) => {
   const { name } = req.query;
   let recipes = await getRecipes(true, false, 300);
+  if (recipes instanceof Error) res.status(400).send(recipes.message);
   if (name) {
     recipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(name.toLowerCase()));
   }
   res.json(recipes);
 });
+
+// recipes.get("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   res.send(id);
+// });
 
 recipes.post("/", async (req, res) => {
   const {
