1965b63fc3d65eeef002458b8959b449d6a8ed2b
Franco Panvini <francopanvini@gmail.com>
2021-09-05 09:35:11 -0300

    add Redux

---
+++ client/src/store/reducer.js
@@ -0,0 +1,73 @@
+import { LOADING, GETRECIPES, PAGINATIONINDEXES, ORDER } from "./actions";
+
+const initialState = { recipes: [], loading: false, indexFirstRecipe: 0, indexLastRecipe: 9 };
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LOADING: {
+      return { ...state, loading: true };
+    }
+    case GETRECIPES: {
+      return { ...state, recipes: action.payload, loading: false };
+    }
+    case PAGINATIONINDEXES: {
+      return {
+        ...state,
+        indexFirstRecipe: action.payload.indexFirstRecipe,
+        indexLastRecipe: action.payload.indexLastRecipe,
+      };
+    }
+    case ORDER: {
+      switch (action.payload) {
+        case "asc": {
+          return {
+            ...state,
+            recipes: state.recipes.sort((a, b) => {
+              if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+              if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+              return 0;
+            }),
+          };
+        }
+        case "desc": {
+          return {
+            ...state,
+            recipes: state.recipes.sort((a, b) => {
+              if (a.title.toLowerCase() < b.title.toLowerCase()) return 1;
+              if (a.title.toLowerCase() > b.title.toLowerCase()) return -1;
+              return 0;
+            }),
+          };
+        }
+        case "score": {
+          return {
+            ...state,
+            recipes: state.recipes.sort((a, b) => {
+              if (a.score < b.score) return 1;
+              if (a.score > b.score) return -1;
+              return 0;
+            }),
+          };
+        }
+        case "healthScore": {
+          return {
+            ...state,
+            recipes: state.recipes.sort((a, b) => {
+              if (a.healthScore < b.healthScore) return 1;
+              if (a.healthScore > b.healthScore) return -1;
+              return 0;
+            }),
+          };
+        }
+        default: {
+          return { ...state };
+        }
+      }
+    }
+    default: {
+      return { ...state };
+    }
+  }
+}
+
+export default reducer;
