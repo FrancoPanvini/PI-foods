ebb8f696acde96b11873cdd00a0a256c028fdfa9
Franco Panvini <francopanvini@gmail.com>
2021-09-10 14:43:39 -0300

    Update getRecipes to 300

--- api/src/services/getRecipes.js
+++ api/src/services/getRecipes.js
@@ -12,11 +12,24 @@ async function getRecipes(name) {
     recipes = await axios.get(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=${number}&cuisine=${cuisine}${name}`
     );
+    recipes = recipes.data.results;
+    if (name === "") {
+      const recipes2 = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=${number}&offset=${number}&cuisine=${cuisine}`
+      );
+      const recipes3 = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=${number}&offset=${
+          2 * number
+        }&cuisine=${cuisine}`
+      );
+      recipes = [...recipes, ...recipes2.data.results, ...recipes3.data.results];
+    }
   } catch (error) {
+    console.log(error.message);
     throw new Error("Problems fetching data from API failed");
   }
 
-  recipes = recipes.data.results.map(recipe => {
+  recipes = recipes?.map(recipe => {
     return {
       id: recipe.id,
       title: recipe.title,
